4b3fbeac884b85daeeb2e7c9cf0338e0693c2fde
siwakorn111246 <siwakorn7565@gmail.com>
2026-02-18 14:28:23 +0700

    Fix database connection to use MongoDB Atlas

--- back-end/src/config/database.js
+++ back-end/src/config/database.js
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 
-const MONGO_URI = "mongodb://127.0.0.1:27017/twopro_db";
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/twopro_db";
+
 
 export const connectDB = async () => {
     try {
